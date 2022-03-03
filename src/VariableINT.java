51c7c0cbf461a958a9366095bfb87d769ca64966
Juan Lima <juan_jpl@hotmail.com>
2022-03-03 19:24:30 -0300

    first commit Variables

---
+++ src/VariableINT.java
@@ -0,0 +1,19 @@
+
+public class VariableINT {
+    public static void main (String args[]){
+        int miVariableEntera = 10 ;
+        //Siempre terminamos las sentencias con punto y coma
+        
+        System.out.println(miVariableEntera);
+        
+        //modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos !! Desde el planeta Tierra";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adiós";
+        System.out.println(miVariableCadena);
+    }
+}
